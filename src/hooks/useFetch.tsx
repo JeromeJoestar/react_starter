e2715e55f040fbedbc34412e8a0ff7eacaa3231c
JeromeJoestar <marcus.ty.parker@gmail.com>
2020-06-26 12:07:12 -0400

    Add types to hook params

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (url, options) => {
+export const useFetch = (url: string, options: any) => {
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
